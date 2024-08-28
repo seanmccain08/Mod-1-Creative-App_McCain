5feed8e6be06868a76f4547c8d2256557947163f
seanmccain08 <seanmccain327@gmail.com>
2024-08-28 07:33:36 -0500

    Initial Commit

---
+++ Mod 1 Creative App_McCain/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mod 1 Creative App_McCain
+//
+//  Created by SEAN MCCAIN on 8/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
