57637028a978c2fd3a294f22159047dcf63c8c10
seanmccain08 <seanmccain327@gmail.com>
2024-09-03 08:09:47 -0500

    changed up UI

--- Mod 1 Creative App_McCain/ViewController.swift
+++ Mod 1 Creative App_McCain/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var itemNameField: UITextField!
+    @IBOutlet weak var itemPriceField: UITextField!
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    var items = ["Apple"]
+    var prices = ["0.50"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
+
     }
 
-
+    @IBAction func addItemButton(_ sender: UIButton) {
+        
+        if(itemNameField.text != "" && itemPriceField.text != ""){
+            
+            items.append(itemNameField.text!)
+            prices.append(itemPriceField.text!)
+            itemNameField.text = ""
+            itemPriceField.text = ""
+            
+        }
+        tableViewOutlet.reloadData()
+        
+    }
+    
 }
 
