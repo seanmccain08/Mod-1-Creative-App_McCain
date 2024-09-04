29ead6bc0415f517c99e18021dddca2f97e80aa8
seanmccain08 <seanmccain327@gmail.com>
2024-09-04 08:06:29 -0500

    Completed App

--- Mod 1 Creative App_McCain/ViewController.swift
+++ Mod 1 Creative App_McCain/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
     @IBOutlet weak var itemNameField: UITextField!
     @IBOutlet weak var itemPriceField: UITextField!
@@ -18,12 +18,49 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        tableViewOutlet.delegate = self
+        tableViewOutlet.dataSource = self
 
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    //populates the table
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableViewOutlet.dequeueReusableCell(withIdentifier: "myCell")!
+        cell.textLabel?.text = items[indexPath.row]
+        cell.detailTextLabel?.text = "$"+prices[indexPath.row]
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete{
+            
+            items.remove(at: indexPath.row)
+            prices.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+        }
+        
+    }
+    
+    @IBAction func clearButton(_ sender: UIButton) {
+        
+        items = []
+        prices = []
+        tableViewOutlet.reloadData()
+        
+    }
+    
     @IBAction func addItemButton(_ sender: UIButton) {
         
+        itemNameField.resignFirstResponder()
+        itemPriceField.resignFirstResponder()
         if(itemNameField.text != "" && itemPriceField.text != ""){
             
             items.append(itemNameField.text!)
@@ -33,6 +70,12 @@ class ViewController: UIViewController {
             
         }
         tableViewOutlet.reloadData()
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
         
     }
     
