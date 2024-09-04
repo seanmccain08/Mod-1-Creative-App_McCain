8c8c018f4fcc6529dd1f1c82dc3023993cf05b95
seanmccain08 <seanmccain327@gmail.com>
2024-09-04 08:06:29 -0500

    Completed App

--- Mod 1 Creative App_McCain/ViewController.swift
+++ Mod 1 Creative App_McCain/ViewController.swift
@@ -7,32 +7,83 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
     @IBOutlet weak var itemNameField: UITextField!
     @IBOutlet weak var itemPriceField: UITextField!
     @IBOutlet weak var tableViewOutlet: UITableView!
+    @IBOutlet weak var errorLabel: UILabel!
     var items = ["Apple"]
     var prices = ["0.50"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        tableViewOutlet.delegate = self
+        tableViewOutlet.dataSource = self
 
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    //populates the table
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableViewOutlet.dequeueReusableCell(withIdentifier: "myCell")!
+        cell.textLabel?.text = items[indexPath.row]
+        cell.detailTextLabel?.text = "$"+prices[indexPath.row]
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete{
+            
+            items.remove(at: indexPath.row)
+            prices.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+        }
+        
+    }
+    
+    @IBAction func clearButton(_ sender: UIButton) {
+        
+        items = []
+        prices = []
+        tableViewOutlet.reloadData()
+        errorLabel.text = " "
+        
+    }
+    
     @IBAction func addItemButton(_ sender: UIButton) {
         
+        itemNameField.resignFirstResponder()
+        itemPriceField.resignFirstResponder()
         if(itemNameField.text != "" && itemPriceField.text != ""){
             
             items.append(itemNameField.text!)
             prices.append(itemPriceField.text!)
             itemNameField.text = ""
             itemPriceField.text = ""
+            errorLabel.text = " "
+            
+        }
+        else{
+            
+            errorLabel.text = "Error: One or more entries are empty"
             
         }
         tableViewOutlet.reloadData()
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
         
     }
     
